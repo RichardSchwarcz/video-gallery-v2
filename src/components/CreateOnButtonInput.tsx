1c47e4f764882ac4af189d21b5b6174c1f3fc0c2
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-01-21 17:33:09 +0100

    button/input component for creating things

---
+++ src/components/CreateOnButtonInput.tsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import { ArrowLeftIcon, CheckIcon, SmallAddIcon } from '@chakra-ui/icons'
+import {
+  Button,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from '@chakra-ui/react'
+
+function CreateOnButtonInput() {
+  const [switchButton, setSwitchButton] = useState(true)
+  const [input, setInput] = useState('')
+
+  function handleToggleClick() {
+    setSwitchButton(!switchButton)
+  }
+
+  return (
+    <>
+      {switchButton && (
+        <Button
+          onClick={() => handleToggleClick()}
+          ml="5"
+          borderRadius="16px"
+          leftIcon={<SmallAddIcon />}
+          variant="solid"
+          colorScheme="purple"
+        >
+          Add New Video
+        </Button>
+      )}
+      {!switchButton && (
+        <InputGroup ml="5">
+          <InputLeftElement>
+            <IconButton
+              size="sm"
+              icon={<ArrowLeftIcon color="gray.500" />}
+              onClick={() => handleToggleClick()}
+              isRound="true"
+              variant="ghost"
+            />
+          </InputLeftElement>
+          <InputRightElement>
+            <IconButton
+              size="sm"
+              icon={<CheckIcon color="gray.100" />}
+              isRound="true"
+              colorScheme="green"
+            />
+          </InputRightElement>
+          <Input
+            onChange={(e) => {
+              setInput(e.target.value)
+            }}
+            pl="14"
+            borderRadius="16px"
+            bg="gray.50"
+            borderColor="gray.100"
+            variant="outline"
+            placeholder="Enter video URL"
+            id="input"
+            boxShadow="lg"
+          />
+        </InputGroup>
+      )}
+    </>
+  )
+}
+
+export default CreateOnButtonInput
