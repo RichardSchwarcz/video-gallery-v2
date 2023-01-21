5e7be996bcecb12530f77142a2627a115b2465f8
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-01-21 17:30:55 +0100

    delete css and some random test file from template

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { ApolloProvider } from '@apollo/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import apolloClient from 'lib/apollo'
 import type { AppProps } from 'next/app'
-import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider>
