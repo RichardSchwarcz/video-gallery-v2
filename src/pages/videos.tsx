b7373030140686edaae02b761ec209fa066f79ce
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-01-23 16:54:45 +0100

    updated videos page

--- src/pages/videos.tsx
+++ src/pages/videos.tsx
@@ -1,7 +1,20 @@
-import NavbarLayout from 'components/NavbarLayout'
+import { Flex } from '@chakra-ui/react'
+import NavbarLayout from 'layouts/NavbarLayout'
+import SearchFilterLayout from 'layouts/SearchFilterLayout'
+import VideosLayout from 'layouts/VideosLayout'
+import SideBar from 'components/SideBar'
 
 function Videos() {
-  return <NavbarLayout />
+  return (
+    <Flex>
+      <SideBar />
+      <Flex direction="column" mx="auto">
+        <NavbarLayout />
+        <SearchFilterLayout />
+        <VideosLayout />
+      </Flex>
+    </Flex>
+  )
 }
 
 export default Videos
