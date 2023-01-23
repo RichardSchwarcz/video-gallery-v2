21c6dc2c3ca36cae1c6ea8846bfdae044d4f6746
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-01-23 16:52:33 +0100

    custom container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,17 @@
+import { Flex } from '@chakra-ui/react'
+
+function Container({ children, my = '0' }) {
+  return (
+    <Flex
+      maxW="1000px"
+      border="1px solid black"
+      px="4"
+      my={my}
+      direction="column"
+    >
+      {children}
+    </Flex>
+  )
+}
+
+export default Container
