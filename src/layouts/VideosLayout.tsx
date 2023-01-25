147814128eb658d91095f3c67ab74c4215be7157
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-01-25 19:57:02 +0100

    create som mock data to test VideoCard component

--- src/layouts/VideosLayout.tsx
+++ src/layouts/VideosLayout.tsx
@@ -3,13 +3,50 @@ import Container from 'components/Container'
 import VideoCard from '../components/VideoCard'
 
 function VideosLayout() {
-  const videoData = ['hehe', 'haha', 'hoho', 'chih', 'chci']
+  const videoData = [
+    {
+      id: '1',
+      title: 'Loading UI with Next.js 13 and React Suspense',
+      url: 'https://www.youtube.com/watch?v=2o5m1ovfl3c',
+      tags: ['react', 'nextjs'],
+      playlists: ['nextjs'],
+    },
+    {
+      id: '2',
+      title: 'Loading UI with Next.js 13 and React Suspense',
+      url: 'https://www.youtube.com/watch?v=2o5m1ovfl3c',
+      tags: ['react', 'nextjs'],
+      playlists: ['nextjs'],
+    },
+    {
+      id: '3',
+      title: 'Loading UI with Next.js 13 and React Suspense',
+      url: 'https://www.youtube.com/watch?v=2o5m1ovfl3c',
+      tags: ['react', 'nextjs'],
+      playlists: ['nextjs'],
+    },
+    {
+      id: '4',
+      title: 'Loading UI with Next.js 13 and React Suspense',
+      url: 'https://www.youtube.com/watch?v=2o5m1ovfl3c',
+      tags: ['react', 'nextjs'],
+      playlists: ['nextjs'],
+    },
+
+    {
+      id: '5',
+      title: 'Loading UI with Next.js 13 and React Suspense',
+      url: 'https://www.youtube.com/watch?v=2o5m1ovfl3c',
+      tags: ['react', 'nextjs'],
+      playlists: ['nextjs'],
+    },
+  ]
 
   return (
     <Container my="4">
       <Flex flexWrap="wrap" gap={2}>
         {videoData.map((element) => (
-          <VideoCard key={element} />
+          <VideoCard key={element.id} video={element} />
         ))}
       </Flex>
     </Container>
