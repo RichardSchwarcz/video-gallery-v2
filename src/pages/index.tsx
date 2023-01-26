801c3e896596ec2e899a9e5a385a5b19ca9855b5
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-01-26 14:52:43 +0100

    update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { Flex } from '@chakra-ui/react'
 import NavbarLayout from 'layouts/NavbarLayout'
+import SearchFilterLayout from 'layouts/SearchFilterLayout'
+import SideBarLayout from 'layouts/SideBarLayout'
 
 function Home() {
-  return <NavbarLayout />
+  return (
+    <Flex>
+      <SideBarLayout />
+      <Flex direction="column" mx="auto">
+        <NavbarLayout />
+        <SearchFilterLayout />
+      </Flex>
+    </Flex>
+  )
 }
 
 export default Home
