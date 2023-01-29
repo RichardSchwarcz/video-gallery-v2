086445ac083ba65b3536c6dc01d5c67eef6a8313
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-01-29 16:22:51 +0100

    get rid of mock data and fetch real from db

--- src/layouts/VideosLayout.tsx
+++ src/layouts/VideosLayout.tsx
@@ -1,52 +1,25 @@
 import { Flex } from '@chakra-ui/react'
+import {
+  useVideosByUserIdQuery,
+  VideosByUserIdQueryResult,
+} from 'generated/generated-graphql'
 import Container from 'components/Container'
 import VideoCard from '../components/VideoCard'
 
 function VideosLayout() {
-  const videoData = [
-    {
-      id: '1',
-      title: 'Loading UI with Next.js 13 and React Suspense',
-      url: 'https://www.youtube.com/watch?v=2o5m1ovfl3c',
-      tags: ['react', 'nextjs'],
-      playlists: ['nextjs'],
+  const { data, loading }: VideosByUserIdQueryResult = useVideosByUserIdQuery({
+    variables: {
+      userById: '851c14c1-72a8-46e3-8141-71394e386a1a',
     },
-    {
-      id: '2',
-      title: 'Loading UI with Next.js 13 and React Suspense',
-      url: 'https://www.youtube.com/watch?v=2o5m1ovfl3c',
-      tags: ['react', 'nextjs'],
-      playlists: ['nextjs'],
-    },
-    {
-      id: '3',
-      title: 'Loading UI with Next.js 13 and React Suspense',
-      url: 'https://www.youtube.com/watch?v=2o5m1ovfl3c',
-      tags: ['react', 'nextjs'],
-      playlists: ['nextjs'],
-    },
-    {
-      id: '4',
-      title: 'Loading UI with Next.js 13 and React Suspense',
-      url: 'https://www.youtube.com/watch?v=2o5m1ovfl3c',
-      tags: ['react', 'nextjs'],
-      playlists: ['nextjs'],
-    },
+  })
 
-    {
-      id: '5',
-      title: 'Loading UI with Next.js 13 and React Suspense',
-      url: 'https://www.youtube.com/watch?v=2o5m1ovfl3c',
-      tags: ['react', 'nextjs'],
-      playlists: ['nextjs'],
-    },
-  ]
+  const videoData = data?.userById?.videos
 
   return (
     <Container my="4">
       <Flex flexWrap="wrap" gap={2}>
-        {videoData.map((element) => (
-          <VideoCard key={element.id} video={element} />
+        {videoData?.map((element) => (
+          <VideoCard key={element?.id} video={element} loading={loading} />
         ))}
       </Flex>
     </Container>
