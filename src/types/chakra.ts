bb1842faf6fbc15135b556d4d8cb147c5f8455cf
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-02-01 15:53:58 +0100

    export Toast types from createOnButton component

--- src/types/chakra.ts
+++ src/types/chakra.ts
@@ -1,4 +1,14 @@
+import { UseToastOptions } from '@chakra-ui/react'
+
 export type DisclosureTypes = {
   isOpen: boolean
   onClose: () => void
+}
+
+export interface Toast extends UseToastOptions {
+  title: string
+  description: string
+  status: 'success' | 'error' | 'warning' | 'info' | 'loading'
+  duration: number
+  isClosable: boolean
 }
