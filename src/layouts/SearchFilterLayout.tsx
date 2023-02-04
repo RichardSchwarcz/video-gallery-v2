37e1a11de35065ee8d288bab817c190ad6db4811
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-02-04 16:12:32 +0100

    type props, pass prop to searchBar

--- src/layouts/SearchFilterLayout.tsx
+++ src/layouts/SearchFilterLayout.tsx
@@ -5,14 +5,22 @@ import SearchBar from 'components/SearchBar'
 import onPathname from 'utils/onPathname'
 import Container from '../components/Container'
 
-function SearchFilterLayout() {
+type SearchFilterLayoutProps = {
+  setSearchInput: React.Dispatch<React.SetStateAction<string>>
+}
+
+function SearchFilterLayout({ setSearchInput }: SearchFilterLayoutProps) {
   const router = useRouter()
   const { pathname } = router
   const { search, paddingLeft } = onPathname(pathname)
   return (
     <Container>
       <Flex gap="4">
-        <SearchBar search={search} paddingLeft={paddingLeft} />
+        <SearchBar
+          search={search}
+          paddingLeft={paddingLeft}
+          setSearchInput={setSearchInput}
+        />
         <Filter />
       </Flex>
     </Container>
