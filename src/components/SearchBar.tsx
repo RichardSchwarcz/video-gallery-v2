75df4e77c40c5243618b2b6a84da05c731687654
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-02-04 16:13:14 +0100

    add clear button, implement clear input logic

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,35 @@
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import { CloseIcon } from '@chakra-ui/icons'
+import {
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from '@chakra-ui/react'
+import apolloClient from 'lib/apollo'
 
 type SearchBarProps = {
   search: string
   paddingLeft: string
+  setSearchInput: React.Dispatch<React.SetStateAction<string>>
 }
 
-function SearchBar({ search, paddingLeft }: SearchBarProps) {
+function SearchBar({ search, paddingLeft, setSearchInput }: SearchBarProps) {
+  const clearInput = () => {
+    const inputElement = document.getElementById('input') as HTMLInputElement
+    if (inputElement !== null) {
+      inputElement.value = ''
+    }
+  }
+
+  const handleClear = () => {
+    void apolloClient.refetchQueries({
+      include: ['UserVideos'],
+    })
+    setSearchInput('')
+    clearInput()
+  }
+
   return (
     <InputGroup mb="5" borderColor="gray.400" m="0">
       <InputLeftElement
@@ -21,7 +45,20 @@ function SearchBar({ search, paddingLeft }: SearchBarProps) {
         pl={paddingLeft}
         borderRadius="16px"
         borderColor="gray.500"
+        id="input"
+        onChange={(e) => setSearchInput(e.target.value)}
       />
+      <InputRightElement>
+        <IconButton
+          aria-label="Clear"
+          icon={<CloseIcon fontSize="2xs" />}
+          size="sm"
+          onClick={() => handleClear()}
+          borderRadius="full"
+        >
+          Clear
+        </IconButton>
+      </InputRightElement>
     </InputGroup>
   )
 }
