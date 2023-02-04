e5d242ac0f1fb76392ee01ba490a628d30a52639
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-02-04 16:09:37 +0100

    debounce input state for search logic

--- src/pages/videos.tsx
+++ src/pages/videos.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { Flex } from '@chakra-ui/react'
 import NavbarLayout from 'layouts/NavbarLayout'
 import SearchFilterLayout from 'layouts/SearchFilterLayout'
@@ -5,13 +6,26 @@ import SideBarLayout from 'layouts/SideBarLayout'
 import VideosLayout from 'layouts/VideosLayout'
 
 function Videos() {
+  const [searchInput, setSearchInput] = useState('')
+  const [debouncedInput, setDebouncedInput] = useState('')
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedInput(searchInput)
+    }, 250)
+
+    return () => {
+      clearTimeout(timerId)
+    }
+  }, [searchInput])
+
   return (
     <Flex>
       <SideBarLayout />
       <Flex direction="column" mx="auto">
         <NavbarLayout />
-        <SearchFilterLayout />
-        <VideosLayout />
+        <SearchFilterLayout setSearchInput={setSearchInput} />
+        <VideosLayout debouncedInput={debouncedInput} />
       </Flex>
     </Flex>
   )
