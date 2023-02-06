94f54480b82e1112f374a64206f46b1eefe76830
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-02-06 16:05:47 +0100

    type user tags add buttons in tags section

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,20 +1,31 @@
-import { ArrowLeftIcon } from '@chakra-ui/icons'
+import {
+  ArrowLeftIcon,
+  ChevronDownIcon,
+  EditIcon,
+  SearchIcon,
+  SmallAddIcon,
+} from '@chakra-ui/icons'
 import {
   Avatar,
   Box,
+  Button,
   Divider,
   Flex,
   Heading,
   IconButton,
+  Input,
   Portal,
   Tag,
+  TagCloseButton,
+  TagLabel,
 } from '@chakra-ui/react'
+import { UserTagsQuery } from 'generated/generated-graphql'
 
 type SideBarProps = {
   isSideBarOpen: boolean
   setIsSideBarOpen: React.Dispatch<React.SetStateAction<boolean>>
   sideBarContainer: React.RefObject<HTMLDivElement>
-  userTags: any
+  userTags: UserTagsQuery | undefined
 }
 
 function SideBar({
@@ -26,7 +37,7 @@ function SideBar({
   return (
     <Portal>
       <Flex position="absolute" top="0" m="4">
-        <Box minW="200px" ref={sideBarContainer} border="1px solid black">
+        <Box minW="200px" ref={sideBarContainer}>
           <Flex alignItems="center" justifyContent="space-between">
             <Flex gap="2" justifyContent="flex-start" alignItems="center">
               <Avatar
@@ -51,12 +62,59 @@ function SideBar({
             <p>Trash</p>
             <Heading size="sm">Tags</Heading>
             <Divider my={2} />
-            <Flex direction="column">
-              {userTags?.userById?.tags.map((tag: any) => (
-                <Tag key={tag.id} colorScheme={tag.color}>
-                  {tag.name}
-                </Tag>
-              ))}
+            <Flex direction="column" gap="2">
+              {/* <Input
+                borderRadius="full"
+                size="sm"
+                placeholder="Create New Tag"
+              /> */}
+              <Flex gap="2">
+                <Button
+                  leftIcon={<SmallAddIcon />}
+                  size="sm"
+                  borderRadius="full"
+                >
+                  New Tag
+                </Button>
+                <Button leftIcon={<SearchIcon />} size="sm" borderRadius="full">
+                  Search
+                </Button>
+              </Flex>
+
+              {userTags && userTags?.userById && userTags?.userById?.tags
+                ? userTags?.userById?.tags.map(
+                    (tag) =>
+                      tag && (
+                        <Flex justifyContent="space-between">
+                          <Tag
+                            key={tag.id}
+                            colorScheme={tag.color}
+                            w="fit-content"
+                            borderRadius="full"
+                          >
+                            <TagLabel>{tag.name}</TagLabel>
+                            <TagCloseButton />
+                          </Tag>
+                          <Flex>
+                            <IconButton
+                              aria-label="EditTag"
+                              icon={<EditIcon />}
+                              size="sm"
+                              variant="ghost"
+                              borderRadius="full"
+                            />
+                            <IconButton
+                              aria-label="change color"
+                              icon={<ChevronDownIcon />}
+                              size="sm"
+                              variant="ghost"
+                              borderRadius="full"
+                            />
+                          </Flex>
+                        </Flex>
+                      )
+                  )
+                : null}
             </Flex>
           </Flex>
         </Box>
