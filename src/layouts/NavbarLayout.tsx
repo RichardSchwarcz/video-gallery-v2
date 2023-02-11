fe3fe0596f6d8d40c8de661cd76b3ca062a6fc5d
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-02-11 21:15:01 +0100

    replace createOnBtn for SwitchBtn component

--- src/layouts/NavbarLayout.tsx
+++ src/layouts/NavbarLayout.tsx
@@ -1,16 +1,49 @@
-import { MoonIcon, SunIcon } from '@chakra-ui/icons'
-import { Flex, IconButton, useColorMode } from '@chakra-ui/react'
+import { MoonIcon, SmallAddIcon, SunIcon } from '@chakra-ui/icons'
+import { Flex, IconButton, useColorMode, useToast } from '@chakra-ui/react'
+import { extractYouTubeVideoInfo } from 'api/utils/getVideoInfo'
+import { useCreateVideoMutation } from 'generated/generated-graphql'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import SwitchButtonInput from 'components/SwitchButtonInput'
 import onPathname from 'utils/onPathname'
 import Container from '../components/Container'
-import CreateOnButtonInput from '../components/CreateOnButtonInput'
+import { ToastBody } from '../utils/toastBody'
 
 function NavbarLayout() {
+  const toast = useToast()
+  const [createVideoMutation, { loading }] = useCreateVideoMutation({
+    refetchQueries: ['UserVideos'],
+    onCompleted() {
+      toast(ToastBody.VideoAdded)
+    },
+  })
   const { colorMode, toggleColorMode } = useColorMode()
   const router = useRouter()
   const { pathname } = router
   const { button, input } = onPathname(pathname)
+
+  const handleCreateVideo = (urlInput: string) => {
+    const videoURL = extractYouTubeVideoInfo(urlInput) // check if url is valid
+
+    if (urlInput === '') {
+      toast(ToastBody.EmptyInput)
+    }
+
+    if (!videoURL.isValid && urlInput !== '') {
+      toast(ToastBody.InvalidURL)
+    }
+
+    if (videoURL.isValid) {
+      void createVideoMutation({
+        variables: {
+          input: {
+            videoUrl: urlInput,
+            userId: '851c14c1-72a8-46e3-8141-71394e386a1a',
+          },
+        },
+      })
+    }
+  }
 
   return (
     <Container my="4">
@@ -32,9 +65,14 @@ function NavbarLayout() {
           icon={colorMode === 'light' ? <SunIcon /> : <MoonIcon />}
           onClick={toggleColorMode}
         />
-        <CreateOnButtonInput
+        <SwitchButtonInput
           buttonPlaceholder={button}
           inputPlaceholder={input}
+          buttonIcon={<SmallAddIcon />}
+          color="green"
+          size="md"
+          loading={loading}
+          handleCreate={handleCreateVideo}
         />
       </Flex>
     </Container>
