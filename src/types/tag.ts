11f5c1977cb46d1b70397e2ada2573849968ff58
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-02-11 21:03:18 +0100

    import generated types, pack them to object

---
+++ src/types/tag.ts
@@ -0,0 +1,5 @@
+import { Tag } from 'generated/generated-graphql'
+
+export type TagType = {
+  tag: Tag
+}
--- src/types/video.ts
+++ src/types/video.ts
@@ -1,16 +1,5 @@
-export type VideoCard = {
-  video: {
-    id: string
-    title: string
-    videoUrl: string
-    author: string
-    authorUrl: string
-    thumbnailUrl: string
-    inTrash: boolean
-    tags: {
-      id: string
-      color: string
-      name: string
-    }[]
-  }
+import { Video } from 'generated/generated-graphql'
+
+export type VideoType = {
+  video: Video
 }
