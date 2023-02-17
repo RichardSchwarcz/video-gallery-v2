8be24a3f3331f2a30477b1cf5c7b2491bf24cfbc
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-02-17 15:20:55 +0100

    videoId not optional, move error handling to top

--- src/api/utils/getVideoInfo.ts
+++ src/api/utils/getVideoInfo.ts
@@ -1,14 +1,15 @@
 interface YouTubeVideoInfo {
   isValid: boolean
-  videoId?: string
+  videoId: string
 }
 
 export const extractYouTubeVideoInfo = (url: string): YouTubeVideoInfo => {
   const youtubeRegex =
     /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/(watch\?v=)?([a-zA-Z0-9_-]+)/
   const matches = youtubeRegex.exec(url)
+
   if (!matches || !matches[5]) {
-    return { isValid: false }
+    return { isValid: false, videoId: '' }
   }
   return { isValid: true, videoId: matches[5] }
 }
@@ -22,20 +23,21 @@ export type VideoDataType = {
 
 export const getVideoData = async (url: string): Promise<VideoDataType> => {
   const videoInfo = extractYouTubeVideoInfo(url)
-  if (videoInfo.isValid && videoInfo.videoId) {
-    const video = `https://noembed.com/embed?url=https://www.youtube.com/watch?v=${videoInfo.videoId}`
-    const response = await fetch(video)
-    const data = await response.json()
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    const { thumbnail_url, title, author_name, author_url }: VideoDataType =
-      data
 
-    return {
-      thumbnail_url,
-      title,
-      author_name,
-      author_url,
-    }
+  if (!videoInfo.isValid) {
+    throw new Error('Invalid YouTube URL')
   }
-  throw new Error('Invalid YouTube URL')
+
+  const video = `https://noembed.com/embed?url=https://www.youtube.com/watch?v=${videoInfo.videoId}`
+  const response = await fetch(video)
+  const data = await response.json()
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const { thumbnail_url, title, author_name, author_url }: VideoDataType = data
+
+  return {
+    thumbnail_url,
+    title,
+    author_name,
+    author_url,
+  }
 }
