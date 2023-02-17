ef697ad66ce14ae1342f3341f2ab6b27708cf259
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-02-17 14:23:08 +0100

    add arguments for later implementation of auth

--- src/api/graphql/context.ts
+++ src/api/graphql/context.ts
@@ -5,8 +5,14 @@ import prisma from '../../lib/prisma'
 export type Context = {
   prisma: PrismaClient
 }
+
+type args = {
+  req: Request
+  res: Response
+}
+
 // eslint-disable-next-line @typescript-eslint/require-await
-export async function createContext(): Promise<Context> {
+export async function createContext({ req, res }: args): Promise<Context> {
   return {
     prisma,
   }
