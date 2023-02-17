8590ae4443c37d272b9f932cf4b3df70334b2d36
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-02-17 14:08:21 +0100

    move query user videos up to videos page

--- src/layouts/VideosLayout.tsx
+++ src/layouts/VideosLayout.tsx
@@ -1,30 +1,13 @@
 import { Flex } from '@chakra-ui/react'
-import { useUserVideosQuery } from 'generated/generated-graphql'
-import apolloClient from 'lib/apollo'
+import { Video } from 'generated/generated-graphql'
 import Container from 'components/Container'
 import VideoCard from '../components/VideoCard'
 
 type VideosLayoutProps = {
-  debouncedInput: string
+  videos: Video[]
 }
 
-function VideosLayout({ debouncedInput }: VideosLayoutProps) {
-  const { data } = useUserVideosQuery({
-    variables: {
-      input: {
-        userId: '851c14c1-72a8-46e3-8141-71394e386a1a',
-        inTrash: false,
-        searchInput: debouncedInput,
-      },
-    },
-    onCompleted: () => {
-      void apolloClient.refetchQueries({
-        include: ['UserVideos'],
-      })
-    },
-  })
-
-  const videos = data?.userVideos?.videos
+function VideosLayout({ videos }: VideosLayoutProps) {
   return (
     <Container my="4">
       <Flex flexWrap="wrap" gap={2}>
--- src/pages/videos.tsx
+++ src/pages/videos.tsx
@@ -1,13 +1,30 @@
 import { useEffect, useState } from 'react'
 import { Flex } from '@chakra-ui/react'
+import { useUserVideosQuery } from 'generated/generated-graphql'
 import NavbarLayout from 'layouts/NavbarLayout'
 import SearchFilterLayout from 'layouts/SearchFilterLayout'
 import SideBarLayout from 'layouts/SideBarLayout'
 import VideosLayout from 'layouts/VideosLayout'
+import apolloClient from 'lib/apollo'
 
 function Videos() {
   const [searchInput, setSearchInput] = useState('')
   const [debouncedInput, setDebouncedInput] = useState('')
+  const { data } = useUserVideosQuery({
+    variables: {
+      input: {
+        inTrash: false,
+        searchInput: debouncedInput,
+      },
+    },
+    onCompleted: () => {
+      void apolloClient.refetchQueries({
+        include: ['UserVideos'],
+      })
+    },
+  })
+
+  const videos = data?.userVideos?.videos
 
   useEffect(() => {
     const timerId = setTimeout(() => {
@@ -24,8 +41,8 @@ function Videos() {
       <SideBarLayout />
       <Flex direction="column" mx="auto">
         <NavbarLayout />
-        <SearchFilterLayout setSearchInput={setSearchInput} />
-        <VideosLayout debouncedInput={debouncedInput} />
+        <SearchFilterLayout handleSearchInput={setSearchInput} />
+        <VideosLayout videos={videos} />
       </Flex>
     </Flex>
   )
