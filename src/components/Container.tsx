64834d5a8b75e65a4a742339c90cf461a1b09362
Richard Schwarcz <riso.schwarcz@gmail.com>
2023-02-17 16:19:30 +0100

    remove padding from container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -10,7 +10,6 @@ function Container({ children, my = '0' }: ContainerProps) {
     <Flex
       maxW="1000px"
       w="1000px"
-      px="4"
       my={my}
       // outline="1px solid black"
       direction="column"
