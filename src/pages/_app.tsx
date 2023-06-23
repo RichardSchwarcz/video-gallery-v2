8512f2131d7cb5f176567e4b2618e77f10cbd7dd
riso.schwarcz <riso.schwarcz@gmail.com>
2023-06-23 16:40:43 +0200

    wrapp app to session provider - auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import { ApolloProvider } from '@apollo/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import apolloClient from 'lib/apollo'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ChakraProvider>
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  </ChakraProvider>
+  <SessionProvider session={pageProps.session}>
+    <ChakraProvider>
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </ChakraProvider>
+  </SessionProvider>
 )
 
 export default MyApp
